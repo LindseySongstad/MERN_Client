ee4d484e80d980bfcf0ca632820c74ee4000e42e
LindseySongstad <lindsey.songstad@outlook.com>
2021-09-24 12:49:45 -0700

    Update
    1st video, 1:13

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,73 @@
-import React from 'react'
+import { useState } from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import FileBase from 'react-file-base64'
+import { useDispatch } from 'react-redux';
+import {createPost} from '../../actions/posts'
+
 import useStyles from './styles';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '', title: '', message: '', tags: '', selectedFile: ''
+    });
     const classes = useStyles();
-    
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+     };
+
+    const clear = () => {};
     return (
-        <div>
-            Form
-        </div>
+        <Paper className={`${classes.root} ${classes.form}}`}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a Memory</Typography>
+                <TextField
+                    name="creator"
+                    variant="outlined"
+                    label="Creator" 
+                    
+                    //value={postData.creator}
+                    fullWidth
+                    onChange={(e) => setPostData({ ...postData, Creator: e.target.value })}
+                />
+                <TextField
+                    name="title"
+                    variant="outlined"
+                    label="Title" 
+                    fullWidth
+                    value={postData.title}
+                    onChange={(e) => setPostData({ ...postData, Title: e.target.value })}
+                />
+                <TextField
+                    name="message"
+                    variant="outlined"
+                    label="Message"
+                    fullWidth
+                    value={postData.message}
+                    onChange={(e) => setPostData({ ...postData, Message: e.target.value })}
+                />
+                <TextField
+                    name="tags"
+                    variant="outlined"
+                    label="Tags"
+                    fullWidth
+                    value={postData.tags}
+                    onChange={(e) => setPostData({ ...postData, Tags: e.target.value })}
+                />
+                <div className={classes.fileInput}>
+                    <FileBase 
+                        type="file"
+                        multiple={false}
+                        onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}
+                    />
+                </div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button  variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     )
 }
 
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,12 +1,14 @@
 
-export default (posts, action) => {
+const posts =  (posts = [], action) => {
     switch(action.type){
         case 'FETCH_ALL':
-            return posts;
-        case 'FETCH_ALL':
-            return posts;
+            return action.payload;
+        case 'CREATE':
+            return [...posts, action.payload];
         default:
             return posts;
         
     }
 }
+
+export default posts;
